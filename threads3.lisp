b795198537b94234c3841ef0f58f26c40dcef6b5
Kalyanov Dmitry <Kalyanov.Dmitry@gmail.com>
2010-08-04 23:03:59 +0400

    Reformat

--- threads3.lisp
+++ threads3.lisp
@@ -1,36 +1,59 @@
 (use-package :sb-thread)
 
+(declaim (optimize (debug 3)) (notinline mm ml ct mct noisefn))
+
+(defun mm ()
+  (cons t t))
+
+(defun ml (mom-mark)
+  (list (make-array 24 :initial-element mom-mark)))
+  
+(defun ct (semaphore parent kid-mark)
+  (wait-on-semaphore semaphore)
+  (let ((old (symbol-value-in-thread 'this-is-new parent)))
+    (setf (symbol-value-in-thread 'this-is-new parent)
+          (make-array 24 :initial-element kid-mark))
+    old))
+    
+(defun mct (semaphore parent kid-mark)
+  (lambda () (ct semaphore parent kid-mark)))
+  
+(defvar *running* nil)  
+
+(defun noisefn ()
+(loop while t
+      do (setf * (make-array 1024))
+      ;; Busy-wait a bit so we don't TOTALLY flood the
+      ;; system with GCs: a GC occurring in the middle of
+      ;; S-V-I-T causes it to start over -- we want that
+      ;; to occur occasionally, but not _all_ the time.
+        (loop repeat (random 128)
+              do (setf ** *))))
+
+;(disassemble 'noisefn)
+              
+(defun run ()
 (let* ((parent *current-thread*)
          (semaphore (make-semaphore))
-         (running t)
-         (noise (make-thread (lambda ()
-                               (loop while running
-                                     do (setf * (make-array 1024))
-                                     ;; Busy-wait a bit so we don't TOTALLY flood the
-                                     ;; system with GCs: a GC occurring in the middle of
-                                     ;; S-V-I-T causes it to start over -- we want that
-                                     ;; to occur occasionally, but not _all_ the time.
-                                        (loop repeat (random 128)
-                                              do (setf ** *)))))))
+         (*running* t)
+         (noise nil))
+    (setf noise (make-thread 'noisefn))
     (write-string "; ")
     (dotimes (i 600000)
       (when (zerop (mod i 20))
         (write-char #\.)
         (force-output))
-      (let* ((mom-mark (cons t t))
-             (kid-mark (cons t t))
-             (child (make-thread (lambda ()
-                                   (wait-on-semaphore semaphore)
-                                   (let ((old (symbol-value-in-thread 'this-is-new parent)))
-                                     (setf (symbol-value-in-thread 'this-is-new parent)
-                                           (make-array 24 :initial-element kid-mark))
-                                     old)))))
-        (progv '(this-is-new) (list (make-array 24 :initial-element mom-mark))
+      (let* ((mom-mark (mm))
+             (kid-mark (mm))
+             (child (make-thread (mct semaphore parent kid-mark))))
+        (progv '(this-is-new) (ml mom-mark)
           (signal-semaphore semaphore)
           (assert (eq mom-mark (aref (join-thread child) 0)))
           (assert (eq kid-mark (aref (symbol-value 'this-is-new) 0))))))
-    (setf running nil)
-    (join-thread noise))
+    (setf *running* nil)
+    (join-thread noise)))
+    
+(run)
     
 (quit)
     
