161be313764e9a247ee23f39b1b84e62eaa0b606
Kalyanov Dmitry <Kalyanov.Dmitry@gmail.com>
2010-10-02 07:06:40 +0400

    Fix typo in contrib/asdf-module.mk

--- contrib/asdf-module.mk
+++ contrib/asdf-module.mk
@@ -16,11 +16,10 @@ endif
 ifeq (CYGWIN,$(findstring CYGWIN,$(UNAME)))
   EXTRA_CFLAGS=-mno-cygwin
   # GCC 4.x doesn't accept -mno-cygwin.
-  CC:=gcc=3
+  CC:=gcc-3
   # SBCL can't read cygwin symlinks, and cygwin likes to symlink
   # gcc.  To further complicate things, SBCL can't handle cygwin
   # paths, either.
-  CC:=gcc-3
   CC:=$(shell cygpath -m $(shell readlink -fn $(shell which $(CC))))
 endif
 
