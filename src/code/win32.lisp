3258fbf3c9081d726b9b5a7d20bf055dd1a0834c
Anton Kovalenko <anton@sw4me.com>
2010-10-16 00:48:32 +0400

    Revert "Check for socket input outside the check for signal state: sockets don't become signalled when they have input."
    This reverts commit f94b091e66d18939ae4cdc066028c826bca41009.

--- src/code/win32.lisp
+++ src/code/win32.lisp
@@ -121,18 +121,20 @@
 (defun handle-listen (handle)
   (with-alien ((avail dword)
                (buf (array char #.input-record-size)))
-    (let ((res (socket-input-available handle)))
-      (unless (zerop res)
-	(return-from handle-listen (= res 1))))
     (when (zerop (wait-for-single-object handle 0))
       (unless
-          (zerop (peek-named-pipe handle nil 0 nil (addr avail) nil))
-        (return-from handle-listen (plusp avail)))
+	  (zerop (peek-named-pipe handle nil 0 nil (addr avail) nil))
+	(return-from handle-listen (plusp avail)))
 
       (unless (zerop (peek-console-input handle
-                                         (cast buf (* t))
-                                         1 (addr avail)))
+					 (cast buf (* t))
+					 1 (addr avail)))
         (return-from handle-listen (plusp avail)))
+
+      (let ((res (socket-input-available handle)))
+	(unless (zerop res)
+	  (return-from handle-listen (= res 1))))
+ 
       t)))
 
 ;;; Listen for input on a C runtime file handle.  Returns true if
