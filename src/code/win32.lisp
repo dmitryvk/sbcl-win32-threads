84a6374378dc40ebda0807f3a195ce22eab86d77
Anton Kovalenko <anton@sw4me.com>
2010-10-24 23:00:27 +0400

    Corrected variable reference

--- src/code/win32.lisp
+++ src/code/win32.lisp
@@ -183,7 +183,7 @@
       (return-from handle-listen (plusp avail)))
     (let ((res (comm-input-available handle)))
       (unless (zerop res)
-	(return-from handle-listen (plusp comm-avail))))
+	(return-from handle-listen (plusp res))))
     (unless (zerop (peek-console-input handle
                                        (cast buf (* t))
                                        1 (addr avail)))
